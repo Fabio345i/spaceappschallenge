0b5bb6e84dd47505b0f3cbeee1708e7d8c466904
dev-t0ny <antoineboudreau@Outlook.com>
2025-10-05 02:35:57 -0400

    commenter les auth

--- api/main.py
+++ api/main.py
@@ -6,8 +6,8 @@ from api.routes.algo import router as algo_router
 from api.routes.weather import rainfall
 from api.db import session
 from dotenv import load_dotenv
-from api.routes.auth.login import router as login_router
-from api.routes.auth.register import router as register_router
+# from api.routes.auth.login import router as login_router
+# from api.routes.auth.register import router as register_router
 from api.routes.merra2 import router as merra2_router
 from api.routes.algo import router as algo_router
 from api.db.session import connect_db, disconnect_db
@@ -45,7 +45,7 @@ def read_root():
 # app.include_router(login_router, prefix="/auth", tags=["Auth"])
 app.include_router(algo_router, prefix="/algo", tags=["Algo"])
 app.include_router(rainfall.router, prefix="/weather", tags=["Weather"])
-app.include_router(register_router, prefix="/register", tags=["Login"])
-app.include_router(login_router, prefix="/login", tags=["Login"])
+# app.include_router(register_router, prefix="/register", tags=["Login"])
+# app.include_router(login_router, prefix="/login", tags=["Login"])
 app.include_router(algo_router, prefix="/algo", tags=["Algo"])
 app.include_router(merra2_router, prefix="/merra2", tags=["MERRA-2"])
